088ec4bb409bee800b9448c2d31abcdd76ecea95
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-13 13:08:58 -0600

    Added Image Directory. Added Code.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="ufc-8">
+		<title>About Me</title>
+	</head>
+	<body>
+		<header>
+		<h1>Jared Padilla</h1>
+		<p>Here are some words talking about me!</p>
+		<h2>just</h2>
+		<h2>testing</h2>
+		<h2>shtuff</h2>
+		<h2>out</h2>
+		<h2>:)</h2>
+		</header>
+
+		<main>
+			<p>paragraph talking about shtuff</p>
+			<p>another paragraph talking about more shtuff</p>
+		</main>
+
+		<footer>
+			<div>&copy; 2016 JPadilla16</div>
+		</footer>
+	</body>
+</html>
