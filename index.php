c956e2f34722c3142e984167286c0ce00654a4a3
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-26 09:59:19 -0600

    Added HTML Content

--- index.php
+++ index.php
@@ -2,18 +2,28 @@
 <html lang="en">
 	<head>
 		<meta charset="ufc-8">
+
+		<!-- Adding CSS! -->
+		<link rel="stylesheet" href="css/style.css"
+				
 		<title>About Me</title>
 	</head>
 	<body>
 		<header>
 		<h1>Jared Padilla</h1>
-		<p>Here are some words talking about me!</p>
+			<div class="container">
+				<a href="#">Here are some words talking about me!</a>
+				<ul>
+					<li><a href="#">Link 1</a></li>
+					<li><a href="#">Link 2</a></li>
+				</ul>
+			</div>
 		<img src="images/IMG_0328.JPG" alt="New Mexico Buildings" height="50%" width="50%"/>
 		</header>
 
 		<main>
-			<p>paragraph talking about shtuff</p>
-			<p>another paragraph talking about more shtuff</p>
+			<p class="width-50">paragraph talking about shtuff</p>
+			<p class="width-50">another paragraph talking about more shtuff</p>
 		</main>
 
 		<footer>
