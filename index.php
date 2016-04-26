08b988f7dc05a3a84e44a355115fe6ce185de7cf
Jared Padilla <jaredpadilla16@gmail.com>
2016-04-26 10:39:41 -0600

    Added Container

--- index.php
+++ index.php
@@ -10,20 +10,19 @@
 	</head>
 	<body>
 		<header>
-		<h1>Jared Padilla</h1>
 			<div class="container">
-				<a href="#">Here are some words talking about me!</a>
+				<a href="#">Jared Padilla</a>
 				<ul>
 					<li><a href="#">Link 1</a></li>
 					<li><a href="#">Link 2</a></li>
 				</ul>
 			</div>
-		<img src="images/IMG_0328.JPG" alt="New Mexico Buildings" height="50%" width="50%"/>
 		</header>
 
 		<main>
-			<p class="width-50">paragraph talking about shtuff</p>
-			<p class="width-50">another paragraph talking about more shtuff</p>
+			<div class="container"
+			<p class="width-50">Vivamus venenatis in felis a venenatis. Mauris dignissim justo nec odio malesuada finibus. Nunc scelerisque ullamcorper nisl, sed elementum sem aliquam id. Quisque vestibulum pellentesque eros non imperdiet. Suspendisse vel quam et lorem tincidunt eleifend eu quis neque. Donec interdum, diam et tincidunt maximus, purus mauris tempor erat, id consequat diam nibh eu felis. Fusce et leo tincidunt dolor semper tincidunt nec et orci. Proin tristique placerat justo ut elementum. Maecenas ut iaculis tellus, sit amet bibendum metus. Praesent quis dapibus ligula. Aliquam interdum facilisis dolor, efficitur interdum nibh feugiat eu. Curabitur lacinia turpis nec condimentum luctus. Donec vitae est sollicitudin, aliquam mi ac, interdum velit. Nulla pellentesque varius blandit. Praesent rutrum porta tristique. Praesent mattis mauris in aliquam rhoncus.</p>
+			<p class="width-50">Morbi vestibulum porta mauris id ornare. Donec posuere congue dolor luctus scelerisque. Nulla eu auctor lacus. Nullam vulputate quis ante et imperdiet. Integer feugiat rhoncus elit, ut vulputate purus interdum vulputate. Proin dictum mollis dolor, vitae bibendum turpis aliquam id. Morbi mollis luctus eros eget scelerisque. Nullam ac porttitor nibh.</p>
 		</main>
 
 		<footer>
